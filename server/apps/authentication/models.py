985d074aa85b1deb31becf4b6adeb21b360d6f83
Rocky Chowdhury <rocky20809@gmail.com>
2025-03-20 16:46:41 +0600

    user address model implemented

--- server/apps/authentication/models.py
+++ server/apps/authentication/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+
+
+
+
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password = None, **kwargs):
@@ -81,3 +86,38 @@ class User(AbstractBaseUser, PermissionsMixin):
     @property
     def is_admin(self):
         return self.role == 'admin'
+    
+
+class Address(models.Model):
+    ADDRESS_TYPES = [
+        ('home', 'Home'),
+        ('work', 'Work'),
+        ('other', 'Other'),
+    ]
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='addresses')
+    address_line1 = models.CharField(max_length=255)
+    address_line2 = models.CharField(max_length=255, blank=True)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=20)
+    country = models.CharField(max_length=100)
+    is_default = models.BooleanField(default=False)
+    address_type = models.CharField(max_length=10, choices=ADDRESS_TYPES, default='home')
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        verbose_name = _('Address')
+        verbose_name_plural = _('Addresses')
+        
+    def __str__(self):
+        return f"{self.address_line1}, {self.city}, {self.postal_code}"
+    
+    def save(self, *args, **kwargs):
+        if self.is_default:
+            Address.objects.filter(user=self.user, is_default=True).update(is_default=False)
+        super().save(*args, **kwargs)
+
