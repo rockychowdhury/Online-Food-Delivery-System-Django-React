15148192e9a0230ce75ebc592f2382a1520a31d6
Rocky Chowdhury <rocky20809@gmail.com>
2025-10-05 14:10:22 +0600

    User model Import issue solved

--- backend/apps/common/models.py
+++ backend/apps/common/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 class Notification(models.Model):
-    user = models.ForeignKey('CustomUser', on_delete=models.CASCADE)
-    message = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    read = models.BooleanField(default=False)
+    user            = models.ForeignKey('accounts.User', on_delete=models.CASCADE)
+    message         = models.TextField()
+    created_at      = models.DateTimeField(auto_now_add=True)
+    read            = models.BooleanField(default=False)
     
     def __str__(self):
         return f"Notification for {self.user}: {self.message[:50]}"
